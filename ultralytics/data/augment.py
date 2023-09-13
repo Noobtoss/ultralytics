b033bffaa2e88cc6eb9b9fff9accb18bec5c0327
Noobtoss <thomasschmittba17196@yahoo.de>
2023-09-13 13:24:41 +0200

    added my changes 2

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -817,7 +817,7 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
     return Compose([
         pre_transform,
         MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
-        Albumentations(p=1.0, hyp=hyp),
+        Albumentations(p=1.0, hyp=hyp), #Semmel
         RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
         RandomFlip(direction='vertical', p=hyp.flipud),
         RandomFlip(direction='horizontal', p=hyp.fliplr, flip_idx=flip_idx)])  # transforms
