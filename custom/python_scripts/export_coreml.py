52ad9a86306ead9858d5f74eea10a342e71f7646
Noobtoss <thomasschmittba17196@yahoo.de>
2025-07-24 13:54:10 +0200

    refactoring

--- custom/python_scripts/export_coreml.py
+++ custom/python_scripts/export_coreml.py
@@ -1,7 +1,24 @@
+import argparse
 from ultralytics import YOLO
 import coremltools as ct
 
-yolo_model = YOLO('../../models/demo_models/SemmelDemo04.pt')  # load yolo_model
 
-yolo_model.export(format="coreml", imgsz=1280, nms=True)  # export yolo_model
-# coreml_model = ct.models.MLModel('resources/Semmel51.mlpackage')  # load coreml_model
+def make_parser():
+    """Create and return the argument parser."""
+    parser = argparse.ArgumentParser(description="YOLO CoreML Export Script")
+    parser.add_argument("--imgsz", type=int, default=1280, help="Image size used during CoreML export")
+    parser.add_argument("--ckpt", type=str, required=True, help="Path to the YOLO model checkpoint (.pt file)")
+    return parser
+
+
+def main():
+    # yolo_model = YOLO('../../models/demo_models/SemmelDemo04.pt')  # load yolo_model
+    # yolo_model.export(format="coreml", imgsz=1280, nms=True)  # export yolo_model
+    # coreml_model = ct.models.MLModel('resources/Semmel51.mlpackage')  # load coreml_model
+    args = make_parser().parse_args()
+    yolo_model = YOLO(args.ckpt)
+    yolo_model.export(format="coreml", imgsz=args.imgsz, nms=True)  # export yolo_model
+
+
+if __name__ == "__main__":
+    main()
