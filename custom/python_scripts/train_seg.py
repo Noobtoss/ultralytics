bc4cb0262b84ce0bc8c7f9d23e8f5a1258538218
Noobtoss <thomasschmittba17196@yahoo.de>
2025-09-10 11:41:57 +0200

    updateing .gitignore

--- custom/python_scripts/train_seg.py
+++ custom/python_scripts/train_seg.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("checkpoints/yolo11x-seg.pt")
 
-results = model.train(data="datasets/holz/holz00baseline.yaml", epochs=100, imgsz=1280)
+results = model.train(data="datasets/holz/holz01noSK.yaml", epochs=100, imgsz=1280)
