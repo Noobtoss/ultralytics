38bf5f9b2a301fa86fb684bcdd051b80908aaf5a
Noobtoss <thomasschmittba17196@gmail.com>
2026-06-02 13:58:14 +0200

    added class call

--- custom/python/mods/rtdetr_trainer.py
+++ custom/python/mods/rtdetr_trainer.py
@@ -1,8 +1,16 @@
-from ultralytics.utils import LOGGER
+from ultralytics.utils import RANK, LOGGER
 from ultralytics.models.rtdetr.train import RTDETRTrainer as _RTDETRTrainer
+
+from .rtdetr_detection_model import RTDETRDetectionModel
 
 
 class RTDETRTrainer(_RTDETRTrainer):
     def __init__(self, *args, **kwargs) -> None:
         LOGGER.warning("[Modded] RTDETRTrainer")
         super().__init__(*args, **kwargs)
+
+    def get_model(self, cfg: dict | None = None, weights: str | None = None, verbose: bool = True):
+        model = RTDETRDetectionModel(cfg, nc=self.data["nc"], ch=self.data["channels"], verbose=verbose and RANK == -1)
+        if weights:
+            model.load(weights)
+        return model
