33b2a73e6951f567abfc2c7ad44f49406ebe0ab3
Noobtoss <thomasschmittba17196@gmail.com>
2026-06-03 13:25:10 +0200

    alignment refactoring

--- custom/python/mods/cls_feat_loss.py
+++ custom/python/mods/cls_feat_loss.py
@@ -44,12 +44,15 @@ class FeatLossFactory:
             return NormalizeEmbeddingsWrapper(losses.CircleLoss(**params, reducer=UnpackReducer()))
 
         elif loss == "multi_sim_loss":
+            # https://kevinmusgrave.github.io/pytorch-metric-learning/losses/#multisimilarityloss
             params = {
                 "alpha": 2.0,
                 "beta": 20.0,
                 "base": 0.5,
             }
-            params.update({k: v for k, v in kwargs.items() if k in inspect.signature(losses.MultiSimilarityLoss).parameters})
+            params.update(
+                {k: v for k, v in kwargs.items() if k in inspect.signature(losses.MultiSimilarityLoss).parameters}
+            )
             return NormalizeEmbeddingsWrapper(losses.MultiSimilarityLoss(**params, reducer=UnpackReducer()))
 
         else:
@@ -60,7 +63,7 @@ class TALAlignWeight:
         self.alpha = alpha
         self.beta = beta
 
-    def __call__(self, pred_scores, target_scores, pred_bboxes, target_bboxes):
+    def __call__(self, target_scores, pred_scores, target_bboxes, pred_bboxes):
         iou = bbox_iou(pred_bboxes, target_bboxes, xywh=False, CIoU=True).squeeze()
         pred_scores = pred_scores[torch.arange(len(pred_scores)), target_scores.argmax(-1)].sigmoid()
         align_metric = pred_scores.pow(self.alpha) * iou.pow(self.beta)
@@ -71,7 +74,7 @@ class ConfWeight:
     def __init__(self, **kwargs):
         pass
 
-    def __call__(self, pred_scores, target_scores, pred_bboxes, target_bboxes):
+    def __call__(self, target_scores, pred_scores, *args, **kwargs):
         return pred_scores.sigmoid().max(-1).values
 
 
@@ -101,14 +104,14 @@ class Masking:
 
 
 class TALAlignMask(Masking, TALAlignWeight):
-    def __call__(self, pred_scores, target_scores, pred_bboxes, target_bboxes):
-        align_metric = super().__call__(pred_scores, target_scores, pred_bboxes, target_bboxes)
+    def __call__(self, target_scores, pred_scores, target_bboxes, pred_bboxes):
+        align_metric = super().__call__(target_scores, pred_scores, target_bboxes, pred_bboxes)
         return self._masking(align_metric)
 
 
 class ConfMask(Masking, ConfWeight):
-    def __call__(self, pred_scores, target_scores, pred_bboxes, target_bboxes):
-        conf = super().__call__(pred_scores, target_scores, pred_bboxes, target_bboxes)
+    def __call__(self, target_scores, pred_scores, *args, **kwargs):
+        conf = super().__call__(target_scores, pred_scores, *args, **kwargs)
         return self._masking(conf)
 
 
@@ -116,10 +119,10 @@ class RandMask:
     def __init__(self, mask_pct: float = 0.4, **kwargs):
         self.mask_pct = mask_pct
 
-    def __call__(self, pred_scores, target_scores):
-        k = max(1, int(len(pred_scores) * self.mask_pct))
-        mask = torch.zeros(len(pred_scores), dtype=torch.bool)
-        indices = torch.randperm(len(pred_scores))[:k]
+    def __call__(self, target_scores, pred_scores=None):
+        k = max(1, int(target_scores.shape[0] * self.mask_pct))
+        mask = torch.zeros(target_scores.shape[0], dtype=torch.bool)
+        indices = torch.randperm(target_scores.shape[0])[:k]
         mask[indices] = True
         return ~mask
 
@@ -129,9 +132,9 @@ class RandMaskBalanced:
         self.mask_pct = mask_pct
         self.min_per_class = min_per_class
 
-    def __call__(self, pred_scores, target_scores):
+    def __call__(self, target_scores, pred_scores=None):
         target_cls = target_scores.max(-1).indices
-        n = len(pred_scores)
+        n = target_scores.shape[0]
         k = max(1, int(n * self.mask_pct))
         mask = torch.zeros(n, dtype=torch.bool)
 
@@ -170,6 +173,7 @@ class MaskFactory:
 class ClsFeatLoss(nn.Module):
     def __init__(self, loss: str, mask: str = None, weight: str = None, **kwargs):
         super().__init__()
+        assert mask is None or weight is None, "Only one of mask or weight can be specified, not both"
         self.loss = FeatLossFactory.get(loss, **kwargs)
         self.mask = MaskFactory.get(mask, **kwargs)
         self.weight = WeightFactory.get(weight, **kwargs)
@@ -177,28 +181,22 @@ class ClsFeatLoss(nn.Module):
     def forward(
             self,
             cls_feats: torch.Tensor,
-            pred_scores: torch.Tensor,
             target_scores: torch.Tensor,
-            pred_bboxes: torch.Tensor,
-            target_bboxes: torch.Tensor
+            *args, **kwargs
     ) -> torch.Tensor:
         loss = torch.tensor(0.0, device=cls_feats.device)
         target_cls = target_scores.max(-1).indices
         if self.mask is not None:
-            mask = self.mask(cls_feats, target_scores, pred_bboxes, target_bboxes)
+            mask = self.mask(cls_feats, target_scores, *args, **kwargs)
             if not mask.sum():
                 return loss
             cls_feats = cls_feats[mask]
             target_cls = target_cls[mask]
-            pred_scores = pred_scores[mask]
-            target_scores = target_scores[mask]
-            pred_bboxes = pred_bboxes[mask]
-            target_bboxes = target_bboxes[mask]
 
         loss_per_element = self.loss(cls_feats, target_cls).squeeze(-1)
 
         if self.weight is not None:
-            weight = self.weight(pred_scores, target_scores, pred_bboxes, target_bboxes)
+            weight = self.weight(target_scores, *args, **kwargs)
             weight = weight / weight.sum()
             loss += (loss_per_element * weight).sum()
         else:
