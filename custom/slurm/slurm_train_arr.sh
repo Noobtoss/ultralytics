e01148b361887197563898df48f2b10a216548b9
Noobtoss <thomasschmittba17196@gmail.com>
2026-06-23 15:37:53 +0200

    init next exp

--- custom/slurm/slurm_train_arr.sh
+++ custom/slurm/slurm_train_arr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=ultralytics_train_arr # Kurzname des Jobs
-#SBATCH --array=5,8%4
+#SBATCH --array=11-16,27-32,43-48,59-64%3
 #SBATCH --output=logs/R-%A-%a.out
 #SBATCH --partition=p2,p4,p6        # p1,p2,p3,p4,p5,p6
 #SBATCH --qos=gpuultimate
